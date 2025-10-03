443aa24a201db1a15fb352503564ade841e80e87
zackariahsgitpage <zackariahwood1273@gmail.com>
2025-10-03 14:38:20 +0100

    coeeficient of fritcion

--- monogame/obj/Debug/net8.0/monogame.AssemblyInfo.cs
+++ monogame/obj/Debug/net8.0/monogame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("monogame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8280e4e6714818b8c1dda0f621b1cd6ac012100")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3abdcee04f544ac393448259e4bbe1430e4fade")]
 [assembly: System.Reflection.AssemblyProductAttribute("monogame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("monogame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
