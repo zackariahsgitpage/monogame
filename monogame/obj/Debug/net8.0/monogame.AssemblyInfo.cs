66be942a0b3a120ef58fd66994b4a975c56f1e82
zackariahsgitpage <zackariahwood1273@gmail.com>
2026-03-23 20:27:25 +0000

    fix fric

--- monogame/obj/Debug/net8.0/monogame.AssemblyInfo.cs
+++ monogame/obj/Debug/net8.0/monogame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("monogame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+578c7e8c255f3eff4e38c9735ad55aaed34aa76b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bc8d4abf70d97434cfe374d3d2c8100cc186847")]
 [assembly: System.Reflection.AssemblyProductAttribute("monogame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("monogame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
