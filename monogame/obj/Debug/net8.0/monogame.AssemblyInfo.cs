5de9177b883244d1f9004b0944299d5bb2ddcfe8
zackariahsgitpage <zackariahwood1273@gmail.com>
2026-04-20 20:49:53 +0100

    final proj

--- monogame/obj/Debug/net8.0/monogame.AssemblyInfo.cs
+++ monogame/obj/Debug/net8.0/monogame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("monogame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe2fab60a66c2e871bea3d0aab76ba19a1f8742a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e95227e52af39f3c1c1b1d70c37e1265f17e7ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("monogame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("monogame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
